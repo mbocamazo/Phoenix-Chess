dd2612b41537be3d35f06f1babe738f5d20dc021
Dennis Chen <dennis.chen@students.olin.edu>
2014-05-11 23:29:27 -0400

    changed open pickled ai list in order to get avg piece weight history

--- open_pickled_ai_list.py
+++ open_pickled_ai_list.py
@@ -19,5 +19,16 @@ import pickle as p
 
 pickle_file = "evolved_AI_pop.p"
 ai_list = p.load(open(pickle_file,"rb"))
-print ai_list[0].piece_weights
-print ai_list[5].piece_weights
+
+history_avg_piece_weights = []
+
+for i in range(len(ai.piece_weights_history)):
+    avg_piece_weights = {'q':0,'r':0,'b':0,'n':0}
+    for ai in ai_list:
+        piece_weights = ai.piece_weights_history[i]
+        for key in piece_weights:
+            avg_piece_weights[key] = avg_piece_weights[key] + piece_weights[key]
+    avg_piece_weights[key] = avg_piece_weights[key]/16.0
+    history_avg_piece_weights.append(avg_piece_weights)
+    
+print history_avg_piece_weights
